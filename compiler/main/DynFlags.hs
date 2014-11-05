0f0371cb77d1455372663d98b95cd3dc9a07e021
Merijn Verstraaten <merijn@inconsistent.nl>
2014-11-05 00:07:55 -0800

    Add -fdefer-typed-holes flag which defers hole errors to runtime.
    Summary:
    As proposed by Richard on Trac. This patch adds a new flag -fdefer-typed-holes
    and changes the semantics of the -fno-warn-typed-holes flag.
    
    To summarise, by default GHC has typed holes enabled and produces a compile
    error when it encounters a typed hole.
    
    When -fdefer-type-errors OR -fdefer-typed-holes is enabled, hole errors are
    converted to warnings and result in runtime errors when evaluated.
    
    The warning flag -fwarn-typed-holes is on by default. Without -fdefer-type-errors
    or -fdefer-typed-holes this flag is a no-op, since typed holes are an error
    under these conditions. If either of the defer flags are enabled (converting
    typed hole errors into warnings) the -fno-warn-typed-holes flag disables the
    warnings. This means compilation silently succeeds and evaluating a hole will
    produce a runtime error.
    
    The rationale behind allowing typed holes warnings to be silenced is that tools
    like Syntastic for vim highlight warnings and hole warnings may be undesirable.
    
    Signed-off-by: Merijn Verstraaten <merijn@inconsistent.nl>
    
    Test Plan: validate
    
    Reviewers: austin
    
    Subscribers: thomie, carter
    
    Differential Revision: https://phabricator.haskell.org/D442
    
    GHC Trac Issues: #9497

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -382,6 +382,7 @@ data GeneralFlag
    | Opt_GhciHistory
    | Opt_HelpfulErrors
    | Opt_DeferTypeErrors
+   | Opt_DeferTypedHoles
    | Opt_Parallel
    | Opt_GranMacros
    | Opt_PIC
@@ -2719,6 +2720,7 @@ fFlags = [
   ( "cmm-sink",                         Opt_CmmSink, nop ),
   ( "cse",                              Opt_CSE, nop ),
   ( "defer-type-errors",                Opt_DeferTypeErrors, nop ),
+  ( "defer-typed-holes",                Opt_DeferTypedHoles, nop ),
   ( "dicts-cheap",                      Opt_DictsCheap, nop ),
   ( "dicts-strict",                     Opt_DictsStrict, nop ),
   ( "dmd-tx-dict-sel",                  Opt_DmdTxDictSel, nop ),
@@ -2999,8 +3001,11 @@ default_PIC platform =
     (OSDarwin, ArchX86_64) -> [Opt_PIC]
     _                      -> []
 
-impliedFlags :: [(ExtensionFlag, TurnOnFlag, ExtensionFlag)]
-impliedFlags
+impliedGFlags :: [(GeneralFlag, TurnOnFlag, GeneralFlag)]
+impliedGFlags = [(Opt_DeferTypeErrors, turnOn, Opt_DeferTypedHoles)]
+
+impliedXFlags :: [(ExtensionFlag, TurnOnFlag, ExtensionFlag)]
+impliedXFlags
 -- See Note [Updating flag description in the User's Guide]
   = [ (Opt_RankNTypes,                turnOn, Opt_ExplicitForAll)
     , (Opt_ScopedTypeVariables,       turnOn, Opt_ExplicitForAll)
@@ -3345,9 +3350,18 @@ setGeneralFlag   f = upd (setGeneralFlag' f)
 unSetGeneralFlag f = upd (unSetGeneralFlag' f)
 
 setGeneralFlag' :: GeneralFlag -> DynFlags -> DynFlags
-setGeneralFlag' f dflags = gopt_set dflags f
+setGeneralFlag' f dflags = foldr ($) (gopt_set dflags f) deps
+  where
+    deps = [ if turn_on then setGeneralFlag'   d
+                        else unSetGeneralFlag' d
+           | (f', turn_on, d) <- impliedGFlags, f' == f ]
+        -- When you set f, set the ones it implies
+        -- NB: use setGeneralFlag recursively, in case the implied flags
+        --     implies further flags
+
 unSetGeneralFlag' :: GeneralFlag -> DynFlags -> DynFlags
 unSetGeneralFlag' f dflags = gopt_unset dflags f
+   -- When you un-set f, however, we don't un-set the things it implies
 
 --------------------------
 setWarningFlag, unSetWarningFlag :: WarningFlag -> DynP ()
@@ -3364,7 +3378,7 @@ setExtensionFlag' f dflags = foldr ($) (xopt_set dflags f) deps
   where
     deps = [ if turn_on then setExtensionFlag'   d
                         else unSetExtensionFlag' d
-           | (f', turn_on, d) <- impliedFlags, f' == f ]
+           | (f', turn_on, d) <- impliedXFlags, f' == f ]
         -- When you set f, set the ones it implies
         -- NB: use setExtensionFlag recursively, in case the implied flags
         --     implies further flags
